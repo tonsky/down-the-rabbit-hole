93b32ba6a74e5c07ef08de73de4e6039e25de700
Nikita Prokopov <niki@tonsky.me>
2021-04-25 20:05:15 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+An entry to [Ludum Dare 48](https://ldjam.com/events/ludum-dare/48)
+
+# Running
+
+You’ll need Python 3, Clojure and JVM installed:
+
+```
+brew install clojure
+./script/run.py
+```
+
+# Screenshot
+
+![screenshot](screenshot.png)
---
+++ src/down_the_rabbit_hole/main.clj
@@ -0,0 +1,85 @@
+(ns down-the-rabbit-hole.main
+  (:require
+   [down-the-rabbit-hole.game :as game])
+  (:import
+   [org.jetbrains.skija BackendRenderTarget Canvas ColorSpace DirectContext FramebufferFormat Paint Rect Surface SurfaceColorFormat SurfaceOrigin]
+   [org.lwjgl.glfw Callbacks GLFW GLFWErrorCallback GLFWKeyCallbackI]
+   [org.lwjgl.opengl GL GL11]
+   [org.lwjgl.system MemoryUtil]))
+
+(set! *warn-on-reflection* true)
+
+(defn color [^long l]
+  (.intValue (Long/valueOf l)))
+
+(def *rect-color (atom (color 0xFFCC3333)))
+
+(defn draw [^Canvas canvas]
+  (let [paint (doto (Paint.) (.setColor @*rect-color))]
+    (.translate canvas 320 240)
+    (.rotate canvas (mod (/ (System/currentTimeMillis) 10) 360))
+    (.drawRect canvas (Rect/makeXYWH -50 -50 100 100) paint)))
+
+(defn display-scale [window]
+  (let [x (make-array Float/TYPE 1)
+        y (make-array Float/TYPE 1)]
+    (GLFW/glfwGetWindowContentScale window x y)
+    [(first x) (first y)]))
+
+(defn -main [& args]
+  (.set (GLFWErrorCallback/createPrint System/err))
+  (GLFW/glfwInit)
+  (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
+  (GLFW/glfwWindowHint GLFW/GLFW_RESIZABLE GLFW/GLFW_TRUE)
+  (let [width 1280
+        height 720
+        window (GLFW/glfwCreateWindow width height "Down the Rabbit Hole" MemoryUtil/NULL MemoryUtil/NULL)]
+    (GLFW/glfwMakeContextCurrent window)
+    (GLFW/glfwSwapInterval 1)
+    (GLFW/glfwShowWindow window)  
+    (GL/createCapabilities)
+
+    (doto (Thread. #(clojure.main/main))
+      (.start))
+
+    (GLFW/glfwSetKeyCallback window
+      (reify GLFWKeyCallbackI
+        (invoke [this _ key _ action mods]
+          (condp = action
+            GLFW/GLFW_PRESS (#'game/on-key-pressed key)
+            nil))))
+
+    (let [context (DirectContext/makeGL)
+          fb-id   (GL11/glGetInteger 0x8CA6)
+          [scale-x scale-y] (display-scale window)
+          target  (BackendRenderTarget/makeGL (* scale-x width) (* scale-y height) 0 8 fb-id FramebufferFormat/GR_GL_RGBA8)
+          surface (Surface/makeFromBackendRenderTarget context target SurfaceOrigin/BOTTOM_LEFT SurfaceColorFormat/RGBA_8888 (ColorSpace/getSRGB))
+          canvas  (.getCanvas surface)]
+      (.scale canvas scale-x scale-y)
+      (loop []
+        (when (not (GLFW/glfwWindowShouldClose window))
+          (.clear canvas (color 0xFFFFFFFF))
+          (let [layer (.save canvas)]
+            (#'game/draw canvas 1280 720)
+            (.restoreToCount canvas layer))
+          (.flush context)
+          (GLFW/glfwSwapBuffers window)
+          (GLFW/glfwPollEvents)
+          (recur)))
+
+      (Callbacks/glfwFreeCallbacks window)
+      (GLFW/glfwHideWindow window)
+      (GLFW/glfwDestroyWindow window)
+      (GLFW/glfwPollEvents)
+
+      (.close surface)
+      (.close target)
+      (.close context)
+
+      (GLFW/glfwTerminate)
+      (.free (GLFW/glfwSetErrorCallback nil))
+      (shutdown-agents)
+)))
+
+(comment
+  (reset! lwjgl.main/*rect-color (lwjgl.main/color 0xFF33CC33)))
